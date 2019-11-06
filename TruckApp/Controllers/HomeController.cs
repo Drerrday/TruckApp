9be2512c7d98a95b26433fa2ab003996c761925c
Hunter Elam <hunterelam21@gmail.com>
2019-11-06 08:52:36 -0600

    Hunter Added Base Code

--- TruckApp/Controllers/HomeController.cs
+++ TruckApp/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace TruckApp.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ TruckApp/Models/Departments.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TruckApp.Models
+{
+    public class Departments
+    {
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+    }
+}
