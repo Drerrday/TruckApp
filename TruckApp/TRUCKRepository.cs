7bc85865c85c95b2d7bd306db6ae8bd274c77e19
Hunter Elam <hunterelam21@gmail.com>
2019-11-07 16:21:04 -0600

    Almost Finished MVC Set

---
+++ TruckApp/TRUCKRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TruckApp.Models;
+namespace TruckApp
+{
+    public class TRUCKRepository
+    {
+        private static string connectionString = System.IO.File.ReadAllText("connectionString.txt");
+        public List<TRUCK> GetAllTRUCKS()
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM TRUCKS;";
+            using (conn)
+            {
+                conn.Open();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                List<TRUCK> allTRUCKS = new List<TRUCK>();
+                while (reader.Read() == true)
+                {
+                    var currentTRUCK = new TRUCK();
+                    currentTRUCK.ID = reader.GetInt32("TRUCKID");
+                    currentTRUCK.Model = reader.GetString("MODEL");
+                    currentTRUCK.Year = reader.GetInt32("YEAR");
+                    currentTRUCK.ReviewID = reader.GetInt32("ReviewID");
+                    allTRUCKS.Add(currentTRUCK);
+                }
+                return allTRUCKS;
+            }
+        }
+    }
+}
